87d31297aca9e74f02d2ee7e18eb5469d7da4d53
Jay Hubert <jaybhubert@gmail.com>
2018-06-29 17:52:02 -0500

    Completed

--- all_eyes_on_you.py
+++ all_eyes_on_you.py
@@ -1,40 +1,64 @@
 # Create a window
 
-# Make eye class if needed
-# Draw eye in window - oval object
-# Draw iris/pupil in eye - black circle on colored circle
+# Make eye class
+    # Draw eyes in window - oval object
+    # Draw irises/pupils in eye - black circle on colored circle
 
-# Get mouse click
+# Indefinite loop
+    # Get mouse click
 
-# Undraw iris/pupil
-# Redraw with x value of center point equal to mouseclick.getX plus radius
+    # Undraw iris/pupil
+    # Redraw with x value of center point equal to mouseclick.getX plus radius
+        #Do not allow to extend beyond boundary of eye
 
 from graphics import *
 
 class Eye:
     def __init__( self, centerPointX, centerPointY, window ):
+        self.centerPointX = centerPointX
+        self.centerPointY = centerPointY
+        self.window = window
+        
         self.eyeLeftEdge = Point( centerPointX - 50, centerPointY - 25 )
         self.eyeRightEdge = Point( centerPointX + 50, centerPointY + 25)
         self.outerEye = Oval( self.eyeLeftEdge, self.eyeRightEdge )
-        self.iris = Circle( Point(centerPointX, centerPointY), 20)
-        self.pupil = Circle( Point(centerPointX, centerPointY), 8)
-        
         self.outerEye.setWidth( 2 )
         self.outerEye.draw( window )
         
+        self.iris = Circle( Point(centerPointX, centerPointY), 20)
         self.iris.setFill( 'blue' )
         self.iris.draw( window )
         
+        self.pupil = Circle( Point(centerPointX, centerPointY), 8)
         self.pupil.setFill( 'black' )
         self.pupil.draw( window )
+              
+    def changeIris( self, newX ):
+        self.iris = Circle( Point( newX, self.centerPointY ), 20)
+        self.iris.setFill( 'blue' )
+        self.iris.draw( self.window )
         
+    def changePupil( self, newX ):
+        self.pupil = Circle( Point( newX, self.centerPointY ), 8)
+        self.pupil.setFill( 'black' )
+        self.pupil.draw( self.window )
+    
     def lookAtMouse( self, X ):
-        #self.iris.undraw()
-        #self.pupil.undraw()
-        if X < self.eyeRightEdge and X > self.eyeLeftEdge:
-            self.iris.move( )
-            self.pupil.move( )
+        self.iris.undraw()
+        self.pupil.undraw()
         
+        if X < self.eyeRightEdge.getX() - 10 and X > self.eyeLeftEdge.getX() + 10:
+            self.changeIris( X )
+            self.changePupil( X )
+            
+        elif X < self.eyeLeftEdge.getX() + 10:
+            self.changeIris( self.centerPointX - 27 )
+            self.changePupil( self.centerPointX - 27 )
+            
+        elif X > self.eyeRightEdge.getX() - 10:
+            self.changeIris( self.centerPointX + 27 )
+            self.changePupil( self.centerPointX + 27 )
+            
 
 def main():
     win = GraphWin( 'All Eyes On You', 400, 400 )
@@ -43,7 +67,7 @@ def main():
     rightEye = Eye( 275, 200, win )
     
     while True:
-        clickPoint = getMouse()
+        clickPoint = win.getMouse()
         leftEye.lookAtMouse( clickPoint.getX() )
         rightEye.lookAtMouse( clickPoint.getX() )
 
